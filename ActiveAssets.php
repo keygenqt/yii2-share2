20440e78cb3722912f5d23d4b9267464cf94916e
keygenqt <keygenqt@gmail.com>
2017-06-23 14:46:43 +0300

    fix

--- ActiveAssets.php
+++ ActiveAssets.php
@@ -9,8 +9,6 @@ use \yii\web\AssetBundle;
  */
 class ActiveAssets extends AssetBundle
 {
-	public $sourcePath = '@keygenqt/share2/assets';
-
 	public $js = [
 		'https://yastatic.net/es5-shims/0.0.2/es5-shims.min.js',
 		'https://yastatic.net/share2/share.js',
